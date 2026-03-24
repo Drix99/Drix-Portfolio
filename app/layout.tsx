25d103ffbbbc04e622c24bb6cab3ed88795b5013
Drix99 <Drixsuarez02@gmail.com>
2026-03-24 14:58:02 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,21 +11,10 @@ export const metadata: Metadata = {
   description: 'My neon-style portfolio showcasing my projects, experience, and skills as a web developer.',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    
+    icon: '/catmeme.jpg', 
+    
+    apple: '/catmeme.jpg', 
   },
 }
 
