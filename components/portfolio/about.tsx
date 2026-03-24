0ff35248cedadefef8a14714d2ca9738f68c40be
Drix99 <Drixsuarez02@gmail.com>
2026-03-24 15:26:32 +0800

    Update about.tsx

--- components/portfolio/about.tsx
+++ components/portfolio/about.tsx
@@ -72,8 +72,7 @@ export default function About(): JSX.Element {
             </p>
             <p>
               When I’m not in the IDE, I’m usually gaming, exploring new frameworks, 
-              or spending time with friends and fam. 
-              I’m ready to take my foundation in web development to a team where I can build, learn, and scale.
+              or spending time with friends and fam.
             </p>
           </div>
         </div>
